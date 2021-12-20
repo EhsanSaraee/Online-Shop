bcb9d90f3294e9ecfa86f4b6ff60a4a1f3d38fe4
ehsan <ehsansaraee6570@yahoo.com>
2021-12-20 12:09:02 +0330

    add cart total price

--- src/Pages/CartPage/CartPage.jsx
+++ src/Pages/CartPage/CartPage.jsx
@@ -2,7 +2,7 @@ import { useCart, useCartDispatch } from '../../Contexts/Cart/CartProvider';
 import './CartPage.css';
 
 const CartPage = () => {
-   const { cart } = useCart();
+   const { cart, total } = useCart();
    const dispatch = useCartDispatch();
 
    const increment = (cartItem) => {
@@ -32,7 +32,10 @@ const CartPage = () => {
                   </div>
                ))}
             </section>
-            <section className="cartSummary">Cart Summary</section>
+            <section className="cartSummary">
+               <h2>Cart Summary</h2>
+               <div>${total}</div>
+            </section>
          </section>
       </main>
    );
