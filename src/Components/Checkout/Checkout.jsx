2a9a46143ab701e35a1a222cbdeea0adf4fc39cb
ehsan <ehsansaraee6570@yahoo.com>
2021-12-24 00:30:31 +0330

    adding styles & optimize checkout

--- src/Components/Checkout/Checkout.jsx
+++ src/Components/Checkout/Checkout.jsx
@@ -1,19 +1,49 @@
+import './checkout.css';
+import { Link } from 'react-router-dom';
 import { useAuth } from '../../Contexts/Auth/AuthProvider';
+import { useCart } from '../../Contexts/Cart/CartProvider';
 
 const Checkout = () => {
    const auth = useAuth();
+   const { cart, total } = useCart();
+
+   if (!cart.length)
+      return (
+         <main className="container">
+            <Link to="/"> go to shopping? </Link>
+         </main>
+      );
+
    return (
-      <>
-         {auth ? (
-            <>
-               <p>Name : {auth.name}</p>
-               <p>Email : {auth.email}</p>
-               <p>Phone : {auth.phoneNumber}</p>
-            </>
-         ) : (
-            <p>Please Login</p>
-         )}
-      </>
+      <main className="container">
+         <section className="cartCenter">
+            {auth ? (
+               <>
+                  <section className="cartItemList">
+                     <h3>checkout detail</h3>
+                     <p>name : {auth.name}</p>
+                     <p>email : {auth.email}</p>
+                     <p>phoneNumber: {auth.phoneNumber}</p>
+                  </section>
+                  <section className="cartSummery">
+                     {cart &&
+                        cart.map((c) => {
+                           return (
+                              <div>
+                                 {c.name} * {c.quantity} :{' '}
+                                 {c.quantity * c.offPrice}
+                              </div>
+                           );
+                        })}
+                     <hr />
+                     <div>total price : {total}</div>
+                  </section>
+               </>
+            ) : (
+               <p>please login !</p>
+            )}
+         </section>
+      </main>
    );
 };
 
